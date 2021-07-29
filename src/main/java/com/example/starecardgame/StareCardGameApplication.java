ae591f9dd46316e456976745886bda3b627b7580
shanshan-su <78042275+shanshan-su@users.noreply.github.com>
2021-07-28 23:01:23 -0500

    feat: created user class

---
+++ src/main/java/com/example/starecardgame/StareCardGameApplication.java
@@ -0,0 +1,13 @@
+package com.example.starecardgame;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StareCardGameApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StareCardGameApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/example/starecardgame/models/User.java
@@ -0,0 +1,96 @@
+package com.example.starecardgame.models;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name="users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(nullable = false, unique = true, length = 50)
+    private String username;
+
+    @Column(nullable = false, unique = true, length = 100)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column
+    private long wins;
+
+    @Column
+    private long loses;
+
+    // Constructors
+
+    public User() {
+    }
+
+    public User(String username, String email, String password) {
+        this.username = username;
+        this.email = email;
+        this.password = password;
+    }
+
+    public User(long id, String username, String email, String password, long wins, long loses) {
+        this.id = id;
+        this.username = username;
+        this.email = email;
+        this.password = password;
+        this.wins = wins;
+        this.loses = loses;
+    }
+
+    // getters and setters
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public long getWins() {
+        return wins;
+    }
+
+    public void setWins(long wins) {
+        this.wins = wins;
+    }
+
+    public long getLoses() {
+        return loses;
+    }
+
+    public void setLoses(long loses) {
+        this.loses = loses;
+    }
+}
