5ff847f2ad162d178186a5f792bd2dd6a0105e9d
shanshan-su <78042275+shanshan-su@users.noreply.github.com>
2021-07-28 23:48:22 -0500

    feat: connected UserController to view

---
+++ src/main/java/com/example/starecardgame/controllers/UserController.java
@@ -0,0 +1,33 @@
+package com.example.starecardgame.controllers;
+
+import com.example.starecardgame.daos.UsersRepository;
+import com.example.starecardgame.models.User;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class UserController {
+    private UsersRepository usersDao;
+    private PasswordEncoder passwordEncoder;
+
+    public UserController(UsersRepository usersDao, PasswordEncoder passwordEncoder) {
+        this.usersDao = usersDao;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    @GetMapping("/")
+    public String showHomePage(Model model) {
+
+        model.addAttribute("title", "Stare Card Game Home");
+        return "home";
+    }
+
+    @GetMapping("/signup")
+    public String showSignupForm(Model model) {
+        model.addAttribute("title", "Signup");
+        model.addAttribute("user", new User());
+        return "users/signup-login";
+    }
+}
---
+++ src/main/java/com/example/starecardgame/daos/UsersRepository.java
@@ -0,0 +1,8 @@
+package com.example.starecardgame.daos;
+
+import com.example.starecardgame.models.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UsersRepository extends JpaRepository<User, Long> {
+    User findByUsername(String username);
+}
