a02dc561e10018d6e809be8da03f2c467247e5bc
shanshan-su <78042275+shanshan-su@users.noreply.github.com>
2021-07-29 22:58:26 -0500

    feat: finished seeder

--- src/main/java/com/example/starecardgame/seeder.sql
+++ src/main/java/com/example/starecardgame/seeder.sql
@@ -3,4 +3,4 @@ USE stare_db;
 INSERT INTO users(id, email, username, password) VALUES (1, 'shanshan@gamil.com', 'shan', 'password'),
                                                         (2, 'james@gamil.com', 'james', 'password');
 
-INSERT INTO cards(id, card_name) VALUES (1, ''),
+INSERT INTO cards(card_name) VALUES ('club 3'), ('club 4'), ('club 5'), ('club 6'), ('club 7'), ('club 8'), ('club 9'), ('club 10'), ('club J'), ('club Q'), ('club K'), ('club A'), ('club 2'), ('spade 3'), ('spade 4'), ('spade 5'), ('spade 6'), ('spade 7'), ('spade 8'), ('spade 9'), ('spade 10'), ('spade J'), ('spade Q'), ('spade K'), ('spade A'), ('spade 2'), ('heart 3'), ('heart 4'), ('heart 5'), ('heart 6'), ('heart 7'), ('heart 8'), ('heart 9'), ('heart 10'), ('heart J'), ('heart Q'), ('heart K'), ('heart A'), ('heart 2'), ('diamond 3'), ('diamond 4'), ('diamond 5'), ('diamond 6'), ('diamond 7'), ('diamond 8'), ('diamond 9'), ('diamond 10'), ('diamond J'), ('diamond Q'), ('diamond K'), ('diamond A'), ('diamond 2'), ('joker 1'), ('joker 2');
